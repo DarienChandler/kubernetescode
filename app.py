c41aa44bf86bdc0414e0c9ffc92da48436792561
DarienChandler <68358848+DarienChandler@users.noreply.github.com>
2022-01-31 12:03:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! Test Change 1.2, Vedika Khanna is great '
+    return 'Hello, World! Test Change 1.2'
+    return 'This is an end to end fully automated pipeline using Jenkins, GitOps, Kubernetes'
+    return 'Proud of you Darien' 
